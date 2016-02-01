14611553b758d21c4885c44bc1e10780b8e4d9fb
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2016-02-01 18:29:29 -0300

    processar multiplos formularios

--- core/form.py
+++ core/form.py
@@ -9,9 +9,34 @@ class PessoaForm(forms.ModelForm):
 
 
 class DeletarForm(forms.Form):
-    pass
+    def __init__(self, id_obj=None, *args, **kwargs):
+        self._id = id_obj
+        super().__init__(*args, **kwargs)
+
+    id = forms.CharField(widget=forms.HiddenInput)
+    hash = forms.CharField(widget=forms.HiddenInput)
+
+    def clean(self):
+        id = self.cleaned_data.get('id')
+        hash = self.cleaned_data.get('hash')
+        if not self._id == id and not id == hash:
+            raise forms.ValidationError(
+                "Hash errado"
+            )
 
 
 class ReValidarForm(forms.Form):
-    pass
+    def __init__(self, id_obj=None, *args, **kwargs):
+        self._id = id_obj
+        super().__init__(*args, **kwargs)
 
+    id = forms.CharField(widget=forms.HiddenInput)
+    hash = forms.CharField(widget=forms.HiddenInput)
+
+    def clean(self):
+        id = self.cleaned_data.get('id')
+        hash = self.cleaned_data.get('hash')
+        if not self._id != id and not id != hash:
+            raise forms.ValidationError(
+                "Hash errado"
+            )
--- core/views.py
+++ core/views.py
@@ -1,4 +1,7 @@
 # Create your views here.
+from django.contrib import messages
+from django.core.urlresolvers import reverse_lazy
+from django.http import JsonResponse
 from django.views import generic
 from .models import Pessoa
 from .form import PessoaForm, ReValidarForm, DeletarForm
@@ -20,17 +23,29 @@ class BaseCreateUpdatePessoa(object):
     prefix = 'pessoa_update'
 
 
-class PessoaCreate(BaseCreateUpdatePessoa, generic.CreateView):
-    template_name = 'core/pessoa_update.html'
+class PessoaCreate(generic.CreateView):
+    model = Pessoa
+    form_class = PessoaForm
+    prefix = 'pessoa_create'
+    template_name = 'core/pessoa_create.html'
+
+    def get_success_url(self):
+        return reverse_lazy('pessoa:update', kwargs={'pk': self.object.pk})
 
 
-class PessoaUpdate(BaseCreateUpdatePessoa, generic.UpdateView):
+class PessoaUpdate(generic.UpdateView):
+    model = Pessoa
+    form_class = PessoaForm
+    prefix = 'pessoa_update'
     template_name = 'core/pessoa_update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('pessoa:update', kwargs={'pk': self.object.pk})
 
     def get_context_data(self, **kwargs):
-        context = super(BaseCreateUpdatePessoa).get_context_data(**kwargs)
-        revalidar_form = ReValidarForm(prefix='revalidar')
-        deletar_form = DeletarForm(prefix='deletar')
+        context = super(PessoaUpdate, self).get_context_data(**kwargs)
+        revalidar_form = ReValidarForm(prefix='revalidar', initial={'hash': self.object.pk, 'id': self.object.pk})
+        deletar_form = DeletarForm(prefix='deletar', initial={'hash': self.object.pk, 'id': self.object.pk})
 
         context.update(
             {
@@ -38,3 +53,40 @@ class PessoaUpdate(BaseCreateUpdatePessoa, generic.UpdateView):
                 'deletar_form': deletar_form
             }
         )
+        return context
+
+    def post(self, request, *args, **kwargs):
+        ret = super(PessoaUpdate, self).post(request, *args, **kwargs)
+
+        revalidar_form = ReValidarForm(data=request.POST, prefix='revalidar', id_obj=self.object.pk)
+        deletar_form = DeletarForm(data=request.POST, prefix='deletar', id_obj=self.object.pk)
+        if revalidar_form:
+            if revalidar_form.is_valid():
+                messages.add_message(request, messages.INFO, 'revalidado')
+                print('revalidado')
+                return JsonResponse({
+                    'mensagem': 'valido revalidado'
+                })
+            else:
+                messages.add_message(request, messages.INFO, 'invalido revalidado')
+                print('invalido  revalidado')
+
+                return JsonResponse({
+                    'mensagem': 'invalido revalidado'
+                })
+
+        if deletar_form:
+            if deletar_form.is_valid():
+                messages.add_message(request, messages.INFO, 'deletado')
+                print('deletado')
+                return JsonResponse({
+                    'mensagem': 'valido deletado'
+                })
+            else:
+                messages.add_message(request, messages.INFO, 'invalido deletado')
+                print('invalido deletado')
+                return JsonResponse({
+                    'mensagem': 'invalido deletado'
+                })
+
+        return ret
